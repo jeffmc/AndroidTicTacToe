f4ad0c88cbd66542781844414870209312d7e3a5
jeff <10823453+jeffmc@users.noreply.github.com>
2022-03-10 10:47:39 -0800

    Fixed app

--- app/src/main/java/com/jeff/androidtictactoe/MainActivity.java
+++ app/src/main/java/com/jeff/androidtictactoe/MainActivity.java
@@ -6,22 +6,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.Observable;
 import java.util.Observer;
 
-public class MainActivity extends AppCompatActivity implements Observer, OnClickListener {
+// Jeff McMillan
+// Java Programming 3/10/22
+// This is an Android TicTacToe app.
+// You play against a (unintelligent) CPU that will try to make moves to hinder your ability to succeed.
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     private TicTacToeGame game;
 
     private Button[][] boardBtns;
 
+    private TicTacToeGame.State playerState = TicTacToeGame.State.X, computerState = TicTacToeGame.State.O;
+
+    // Setup program!
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ttt);
         game = new TicTacToeGame();
-        game.addObserver(this);
         boardBtns = new Button[][]{
             new Button[]{
                 findViewById(R.id.button00),
@@ -42,22 +50,17 @@ public class MainActivity extends AppCompatActivity implements Observer, OnClick
                 btn.setOnClickListener(this); // add listeners
                 btn.setText("");
         }
-        try {
-            game.makeMove(0,0, TicTacToeGame.State.X);
-        } catch (TicTacToeGame.IllegalMoveException e) {
-//            e.printStackTrace();
-            System.err.println(e.getMessage());
-        }
+//        try {
+//            game.makeMove(0,0, playerState);
+//        } catch (TicTacToeGame.IllegalMoveException e) {
+////            e.printStackTrace();
+//            System.err.println(e.getMessage());
+//        }
         game.print();
         setButtonsToGame();
     }
 
-    @Override
-    public void update(Observable observable, Object arg) {
-        System.out.println("OBSERVED!");
-        setButtonsToGame();
-    }
-
+    // Listens for all clicks on button grid
     @Override
     public void onClick(View view) {
 //        Thread thr = Thread.currentThread();
@@ -66,27 +69,38 @@ public class MainActivity extends AppCompatActivity implements Observer, OnClick
         tryMoveWithButton(btn);
     }
 
+    // Reset visual state of the board
     public void setButtonsToGame() {
         for (int i=0;i < boardBtns.length; i++) {
             Button[] col = boardBtns[i];
             for (int j=0;j < col.length; j++) {
-                col[j].setText(game.getStateAtPos(i,j).str());
+                TicTacToeGame.State s = game.getStateAtPos(i,j);
+                col[j].setText(s.str());
+                col[j].setEnabled(s == TicTacToeGame.State.Empty);
             }
         }
     }
 
+    // Try a move on the ttt board from this button.
     public void tryMoveWithButton(Button btn) {
         int[] pos = findInBoard(btn);
-        System.out.println(pos[0] + ", " + pos[1]);
         try {
-            game.makeMove(pos[0], pos[1], TicTacToeGame.State.O);
-            btn.setText("O");
+            game.makeMove(pos[0], pos[1], playerState);
+            game.computerMove(computerState);
         } catch (TicTacToeGame.IllegalMoveException e) {
             e.printStackTrace();
-            btn.setText("E");
+//            btn.setText("E");
         }
+        TicTacToeGame.State winner = game.gameWon();
+        if (winner != null) {
+            restartGame();
+            String msg = (winner == TicTacToeGame.State.Empty) ? "CAT Game!" : winner.str() + " won the game!";
+            Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_LONG).show();
+        }
+        setButtonsToGame();
     }
 
+    // Find button's position in the TTT 2d array
     public int[] findInBoard(View view) {
         for (int i=0;i<boardBtns.length;i++) {
             Button[] col = boardBtns[i];
@@ -95,5 +109,10 @@ public class MainActivity extends AppCompatActivity implements Observer, OnClick
                     return new int[]{i, j};
         }
         return new int[]{-1,-1};
+    }
+
+    // Reset state of board DOESN'T REFRESH UI!
+    public void restartGame() {
+        game = new TicTacToeGame();
     }
 }
--- app/src/main/java/com/jeff/androidtictactoe/TicTacToeGame.java
+++ app/src/main/java/com/jeff/androidtictactoe/TicTacToeGame.java
@@ -1,9 +1,11 @@
 package com.jeff.androidtictactoe;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Observable;
+import java.util.TreeMap;
 
-public class TicTacToeGame extends Observable {
+public class TicTacToeGame {
 
     private State[] board;
     public static final int bsize = 3;
@@ -11,26 +13,72 @@ public class TicTacToeGame extends Observable {
     public TicTacToeGame() {
         board = new State[bsize*bsize];
         Arrays.fill(board, State.Empty);
-        notifyObservers();
     }
 
     public TicTacToeGame(TicTacToeGame ref) {
         board = ref.board.clone();
-        notifyObservers();
     }
 
+    // Affect the board using this player
     public void makeMove(int x, int y, State s) throws IllegalMoveException {
+        State winner = gameWon();
+        if (winner != null) throw new IllegalMoveException(winner, this);
         if (s == null || s == State.Empty || x < 0 || y < 0 || x >= bsize || y >= bsize) throw new IllegalMoveException(x, y, s,this);
         int i = bsize*y + x;
         if (board[i] != State.Empty) throw new IllegalMoveException(x,y,s,this);
         board[i] = s;
-        notifyObservers();
+    }
+
+    public void classicComputerMove(State s) throws IllegalMoveException {
+        State winner = gameWon();
+        if (winner != null) throw new IllegalMoveException(winner, this);
+        for (int x=0;x<bsize;x++) {
+            for (int y=0;y<bsize;y++) {
+                try {
+                    makeMove(x, y, s);
+                    return;
+                } catch (IllegalMoveException e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+        }
+    }
+
+    // Goal with computer move is to make the most impactful move by placing the next move where the most adjacent cells are filled.
+    public void computerMove(State s) throws IllegalMoveException {
+        State winner = gameWon();
+        if (winner != null) throw new IllegalMoveException(winner, this);
+        TreeMap<Integer, int[]> moves = new TreeMap<>();
+        for (int x=0;x<bsize;x++) {
+            for (int y=0;y<bsize;y++) {
+                if (getStateAtPos(x,y) == State.Empty) {
+                    moves.put(countAdjacency(x,y), new int[]{x,y});
+                }
+            }
+        }
+        int[] move = moves.descendingMap().firstEntry().getValue();
+        makeMove(move[0], move[1], s);
+    }
+
+
+    public int countAdjacency(int x, int y) { //Returns number of cells filled by computer or player adjacently (diagonals included)
+        final int[] offsets = new int[]{-bsize-1,-bsize, -bsize+1,-1,1,bsize-1,bsize,bsize+1}; // ALL 8 spots around chosen
+        int oi = y*bsize + x; // original index
+        int a = 0;
+        for (int o=0;o< offsets.length;o++) {
+            int i = oi + offsets[o];
+            if (i > -1 && i < board.length) {
+                if (getStateAtPos(i) != State.Empty) a++;
+            }
+        }
+        return a;
     }
 
     public State getStateAtPos(int i) {
         return board[i];
     }
 
+    // Print to console
     public void print() {
         for (int o=0;o<bsize*bsize;o+=bsize) {
             System.out.println("[ " + board[o].str() + " " + board[o+1].str() + " " + board[o+1].str() + " ]");
@@ -59,16 +107,22 @@ public class TicTacToeGame extends Observable {
         return State.Empty; // CAT game
     }
 
+    // Used to describe state of TTT grid.
     public enum State {
         X("X"), O("O"), Empty("-");
         private String str;
         State(String s) { str = s; }
         public String str() { return str; }
-    }
 
+
+    }
+    // Exceptions for making moves, to be handled by view
     public class IllegalMoveException extends Exception {
         public IllegalMoveException(int x, int y, State s, TicTacToeGame game) {
-            super(x + ", " + y + ": " + s.name());
+            super("INVALID MOVE: " + x + ", " + y + ": " + s.str());
+        }
+        public IllegalMoveException(State s, TicTacToeGame game) {
+            super("GAME OVER: " + s.str() + " already won the game!");
         }
     }
 }
